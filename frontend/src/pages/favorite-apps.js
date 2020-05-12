7c1d3fbf998ba08e811040117c47753f2fcfb1c1
Andrew Horn <arhorn@smcm.edu>
2020-05-12 17:34:10 -0500

    added skeleton pages

---
+++ frontend/src/pages/favorite-apps.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const FavoriteAppsPage = () => (
+  <Layout>
+    <SEO title="Favorite Apps" />
+    <h1>favorite apps</h1>
+  </Layout>
+)
+
+export default FavoriteAppsPage
---
+++ frontend/src/pages/profile-page.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ProfilePage = () => (
+  <Layout>
+    <SEO title="Profile Page" />
+    <h1>profile page</h1>
+  </Layout>
+)
+
+export default ProfilePage
