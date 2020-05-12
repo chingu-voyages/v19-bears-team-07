bfb16d97dd50c694eac2091b1327db6e7c882606
Andrew Horn <arhorn@smcm.edu>
2020-05-12 17:19:10 -0500

    removed top navbar toggle functionality

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react"
+import React from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAlignLeft } from "@fortawesome/free-solid-svg-icons"
-import {
-  Navbar,
-  Button,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-} from "reactstrap"
+import { Navbar, Button, Nav, NavItem, NavLink } from "reactstrap"
 import { Link } from "gatsby"
 
-export default props => {
-  const [isOpen, setOpen] = useState(true)
-  const toggle = () => setOpen(!isOpen)
-
+export default ({ toggle }) => {
   return (
     <Navbar
       color="light"
@@ -23,19 +12,16 @@ export default props => {
       className="navbar shadow-sm p-3 mb-5 bg-white rounded"
       expand="md"
     >
-      <Button color="info" onClick={props.toggle}>
+      <Button color="info" onClick={toggle}>
         <FontAwesomeIcon icon={faAlignLeft} />
       </Button>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink tag={Link} to={"/login"}>
-              sign up / login
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink tag={Link} to={"/login"}>
+            sign up / login
+          </NavLink>
+        </NavItem>
+      </Nav>
     </Navbar>
   )
 }
