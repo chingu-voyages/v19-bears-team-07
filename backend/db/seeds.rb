0afae22e1b232be4d3d1b3fc18634bee6a4c0cb5
Matt <smtrussell@gmail.com>
2020-05-12 18:58:11 -0500

    seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+apps = App.create([{ name: 'Facebook', description: 'Social Network'}, { name: 'Twitter', description: 'Tweets'}])
+
+tags = Tag.create([{ name: 'Games', description: 'Play Time'}, { name: 'Productivity', description: 'Work Stuff'}])
