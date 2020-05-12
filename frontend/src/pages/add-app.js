fcafac4134f8721f656182f1d3a409ad9aafbcb9
Andrew Horn <arhorn@smcm.edu>
2020-05-12 16:55:01 -0500

    added add-app page

---
+++ frontend/src/pages/add-app.js
@@ -0,0 +1,12 @@
+import React from "react"
+import AddApp from "../components/AddApp/AddApp"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+const Page2 = () => (
+  <div>
+    <AddApp />
+  </div>
+)
+
+export default Page2
