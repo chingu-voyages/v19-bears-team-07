70dbc12e7afddb80182481600d7ebe0849566a94
Andrew Horn <arhorn@smcm.edu>
2020-05-18 20:17:36 -0500

    refactored UserGrid into three components

--- frontend/src/components/UserGrid/UserGrid.js
+++ frontend/src/components/UserGrid/UserGrid.js
@@ -3,34 +3,32 @@ import { navigate } from "gatsby"
 import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-const UserGrid = props => {
-  const { users, className } = props
+const UserGrid = ({ users }) => (
+  <div className={"UserGrid-container"}>
+    <RenderUsers users={users} />
+  </div>
+)
 
-  return (
-    <div className={"UserGrid-container" + (className ? " " + className : "")}>
-      {renderUsers()}
-    </div>
-  )
+const RenderUsers = ({ users }) => (
+  <React.Fragment>
+    {users.map((user, index) => (
+      <RenderSingleUser user={user} index={index} />
+    ))}
+  </React.Fragment>
+)
 
-  function renderUsers() {
-    const cards = users.map((user, index) => {
-      const { image, name, bio, url } = user
-      return (
-        <Card
-          onClick={() => navigate(url)}
-          className={"UserGrid-Card"}
-          key={index.toString() + "-" + url}
-        >
-          <CardImg src={image} width={"100%"}></CardImg>
-          <CardBody>
-            <CardTitle>{name}</CardTitle>
-            <CardText>{bio}</CardText>
-          </CardBody>
-        </Card>
-      )
-    })
-    return <React.Fragment>{cards}</React.Fragment>
-  }
-}
+const RenderSingleUser = ({ user: { image, name, bio, url }, index }) => (
+  <Card
+    onClick={() => navigate(url)}
+    className={"UserGrid-Card"}
+    key={index.toString() + "-" + url}
+  >
+    <CardImg src={image} width={"100%"}></CardImg>
+    <CardBody>
+      <CardTitle>{name}</CardTitle>
+      <CardText>{bio}</CardText>
+    </CardBody>
+  </Card>
+)
 
 export default UserGrid
