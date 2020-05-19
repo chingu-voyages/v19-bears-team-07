a2ce667fbbc5e0acc32ee6166bcd90f40629d609
Andrew Horn <arhorn@smcm.edu>
2020-05-18 23:27:15 -0500

    updated url names ALL_APPS -> APPS, ALL_USERS -> USERS

--- frontend/src/shared/urls.js
+++ frontend/src/shared/urls.js
@@ -2,8 +2,8 @@ export const BACKEND_HOST = "http://localhost:3000"
 export const SIGN_IN = `${BACKEND_HOST}/auth/sign_in`
 export const CHECK_LOGGED_IN = `${BACKEND_HOST}/test_login/is_logged_in`
 export const LOG_OUT = `${BACKEND_HOST}/auth/sign_out`
-export const ALL_APPS = `${BACKEND_HOST}/apps`
-export const ALL_USERS = `${BACKEND_HOST}/users`
+export const APPS = `${BACKEND_HOST}/apps`
+export const USERS = `${BACKEND_HOST}/users`
 
 export const singleApp = appId => `${BACKEND_HOST}/apps/${appId}`
 export const singlePortfolio = userId =>
