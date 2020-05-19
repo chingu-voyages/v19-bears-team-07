d989a80217c7a684c999128dcdc124fe1ce75fe7
Howard Chen <hchen7913@gmail.com>
2020-05-19 12:07:15 -0700

    Add migration so that deleting an app cascade-deletes down to the tags

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_204139) do
+ActiveRecord::Schema.define(version: 2020_05_19_183920) do
 
   create_table "apps", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,6 @@ ActiveRecord::Schema.define(version: 2020_05_13_204139) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "apps", "users"
-  add_foreign_key "tags", "apps"
+  add_foreign_key "apps", "users", on_delete: :cascade
+  add_foreign_key "tags", "apps", on_delete: :cascade
 end
