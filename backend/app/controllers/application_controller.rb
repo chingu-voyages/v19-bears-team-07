77b6f5d01638e55ed91a071f108b06e76d1f5931
Matt <smtrussell@gmail.com>
2020-05-21 17:57:29 -0500

    prod files

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   # This line needs to be included to allow cookie-based authentication
   # from all white-listed domains to succeed without HTML forms
   protect_from_forgery unless: -> {
-    puts base_url
     base_url = request.base_url
+    puts base_url
     my_config = Rails.application.config
     (base_url.include? my_config.client_root) or (base_url.include? my_config.server_root)
   }
