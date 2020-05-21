beeb8a685dc95aabbb6132a03ea5b2220472639d
Andrew Horn <arhorn@smcm.edu>
2020-05-21 16:28:45 -0500

    set url for remote backend

--- frontend/src/shared/urls.js
+++ frontend/src/shared/urls.js
@@ -1,4 +1,5 @@
-export const BACKEND_HOST = "http://localhost:3000"
+// export const BACKEND_HOST = "http://localhost:3000"
+export const BACKEND_HOST = "https://afternoon-sea-83763.herokuapp.com"
 export const SIGN_IN = `${BACKEND_HOST}/auth/sign_in`
 export const CHECK_LOGGED_IN = `${BACKEND_HOST}/test_login/is_logged_in`
 export const LOG_OUT = `${BACKEND_HOST}/auth/sign_out`
