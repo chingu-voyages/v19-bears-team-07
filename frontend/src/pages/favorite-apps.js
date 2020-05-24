52dcc47198a3c24d55b7e7f7a5ebdee07e6bcfe4
Howard Chen <hchen7913@gmail.com>
2020-05-24 13:38:59 -0700

    Add favorite button that can be clicked to toggle the favoriting

--- frontend/src/pages/favorite-apps.js
+++ frontend/src/pages/favorite-apps.js
@@ -3,11 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import Layout from "../components/Layout/Layout"
 import SEO from "../components/Seo/Seo"
+import FavoriteAppGrid from "../components/FavoriteAppGrid/FavoriteAppGrid"
 
 const FavoriteAppsPage = () => (
   <Layout>
     <SEO title="Favorite Apps" />
     <h1>favorite apps</h1>
+    <FavoriteAppGrid></FavoriteAppGrid>
   </Layout>
 )
 
