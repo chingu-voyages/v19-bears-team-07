ff4c111eac21684dc42fecae7faa1e9a5b545e13
Howard Chen <hchen7913@gmail.com>
2020-06-07 14:39:06 -0700

    Update metadata and provide redirect info for netlify

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `DevHub`,
+    description: `Browse interesting apps and devs`,
+    author: `Bears-7`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- frontend/src/components/share-social.js
+++ frontend/src/components/share-social.js
@@ -7,16 +7,20 @@ import {
   LinkedinShareButton,
   TwitterShareButton,
 } from "react-share"
+import { Helmet } from "react-helmet"
 import { useLocation } from "@reach/router"
 
 // Share icons will share the current page unless the props override it.
 // TODO: To expand what these social icons do, use the docs at https://github.com/nygardk/react-share
-export const ShareSocial = ({ shareUrl }) => {
+export const ShareSocial = ({ shareUrl, imageUrl }) => {
   const location = useLocation()
   const href = shareUrl ? shareUrl : location.href
 
   return (
     <div className={"ShareSocial-Container"}>
+      <Helmet>
+        <meta property="og:image" content={imageUrl} />
+      </Helmet>
       <FacebookShareButton url={href}>
         <FacebookIcon round></FacebookIcon>
       </FacebookShareButton>
